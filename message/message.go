4741d5da454a5ebe2dd9835ebfae2296f57bb50f
Michal Bock <michal.bock@gmail.com>
2019-11-23 10:47:27 +0000

    Rename byte message to message.

--- message/message.go
+++ message/message.go
@@ -1,22 +1,27 @@
 package message
 
-// ByteMessage implements substrate.DiscardableMessage interface by
+// Message implements substrate.DiscardableMessage interface by
 // returning the payload on a call to the data method.
-type ByteMessage struct {
+type Message struct {
 	Payload []byte
 }
 
-// NewByteMessage returns a new instance of byte message.
-func NewByteMessage(payload []byte) *ByteMessage {
-	return &ByteMessage{Payload: payload}
+// NewMessage returns a new instance of message.
+func NewMessage(payload []byte) *Message {
+	return &Message{Payload: payload}
+}
+
+// FromString convenience method to initialise a byte message from string.
+func FromString(payload string) *Message {
+	return &Message{Payload: []byte(payload)}
 }
 
 // Data returns the payload.
-func (msg *ByteMessage) Data() []byte {
+func (msg *Message) Data() []byte {
 	return msg.Payload
 }
 
 // DiscardPayload discards the payload.
-func (msg *ByteMessage) DiscardPayload() {
+func (msg *Message) DiscardPayload() {
 	msg.Payload = nil
 }
