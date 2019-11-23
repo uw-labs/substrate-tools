f0c06ed21ab5118348926fd179eae9151df3c378
Michal Bock <michal.bock@gmail.com>
2019-11-23 10:31:44 +0000

    Add proper closing to mock message source.

--- mock/source.go
+++ mock/source.go
@@ -2,24 +2,35 @@ package mock
 
 import (
 	"context"
+	"errors"
+	"sync"
 
 	"github.com/uw-labs/substrate"
 )
 
-// NewAsyncMessageSource returns a message source that sends the provided messages to the user.
+var _ substrate.AsyncMessageSource = (*AsyncMessageSource)(nil)
+
+// AsyncMessageSource is a message source that sends the provided messages to the user.
 // Waits for acknowledgements and then terminates. It expects acknowledgements to be in order
 // in which the messages were written.
-func NewAsyncMessageSource(msgs []substrate.Message) substrate.AsyncMessageSource {
-	return mockSource{
-		messages: msgs,
+type AsyncMessageSource struct {
+	// Messages that will be sent to the client.
+	Messages []substrate.Message
+
+	mutex  sync.RWMutex
+	ctx    context.Context
+	cancel func()
+	closed bool
+}
+
+// ConsumeMessages consumes messages sends all messages in the underlying slice to the client, waits for all of them
+// to be acknowledged in the correct order and exits. It will error in case the source was already closed.
+func (s *AsyncMessageSource) ConsumeMessages(ctx context.Context, messages chan<- substrate.Message, acks <-chan substrate.Message) error {
+	ctx, err := s.init(ctx)
+	if err != nil {
+		return err
 	}
-}
 
-type mockSource struct {
-	messages []substrate.Message
-}
-
-func (s mockSource) ConsumeMessages(ctx context.Context, messages chan<- substrate.Message, acks <-chan substrate.Message) error {
 	toWrite, toAck := 0, -1
 
 	checkAck := func(msg substrate.Message) error {
@@ -29,21 +40,21 @@ func (s mockSource) ConsumeMessages(ctx context.Context, messages chan<- substra
 				Acked:    msg,
 			}
 		}
-		if msg != s.messages[toAck] {
+		if msg != s.Messages[toAck] {
 			return substrate.InvalidAckError{
 				Acked:    msg,
-				Expected: s.messages[toAck],
+				Expected: s.Messages[toAck],
 			}
 		}
 		toAck++
 		return nil
 	}
 
-	for toWrite < len(s.messages) {
+	for toWrite < len(s.Messages) {
 		select {
 		case <-ctx.Done():
 			return nil
-		case messages <- s.messages[toWrite]:
+		case messages <- s.Messages[toWrite]:
 			toWrite++
 			if toAck == -1 {
 				toAck = 0
@@ -54,7 +65,7 @@ func (s mockSource) ConsumeMessages(ctx context.Context, messages chan<- substra
 			}
 		}
 	}
-	for toAck < len(s.messages) {
+	for toAck < len(s.Messages) {
 		select {
 		case <-ctx.Done():
 			return nil
@@ -68,10 +79,56 @@ func (s mockSource) ConsumeMessages(ctx context.Context, messages chan<- substra
 	return nil
 }
 
-func (s mockSource) Close() error {
+func (s *AsyncMessageSource) init(ctx context.Context) (context.Context, error) {
+	s.mutex.Lock()
+	defer s.mutex.Unlock()
+
+	if s.ctx != nil {
+		return nil, errors.New("source already in use")
+	}
+	if s.closed {
+		return nil, errors.New("source already closed")
+	}
+	s.ctx, s.cancel = context.WithCancel(ctx)
+
+	return s.ctx, nil
+}
+
+// Close closes the message source it will cause the call to ConsumeMessages to return.
+func (s *AsyncMessageSource) Close() error {
+	s.mutex.Lock()
+	defer s.mutex.Unlock()
+
+	if s.closed {
+		return nil
+	}
+	s.closed = true
+
+	if s.cancel != nil {
+		s.cancel()
+	}
 	return nil
 }
 
-func (s mockSource) Status() (*substrate.Status, error) {
-	return &substrate.Status{Working: true}, nil
+// Status returns the status of this message source. It will report not working after the source was closed.
+func (s *AsyncMessageSource) Status() (*substrate.Status, error) {
+	s.mutex.RLock()
+	defer s.mutex.RUnlock()
+
+	if !s.closed {
+		return &substrate.Status{Working: true}, nil
+	}
+
+	return &substrate.Status{
+		Working:  false,
+		Problems: []string{"sink already closed"},
+	}, nil
+}
+
+// WasClosed indicates whether the close method was called on the message source.
+func (s *AsyncMessageSource) WasClosed() bool {
+	s.mutex.RLock()
+	defer s.mutex.RUnlock()
+
+	return s.closed
 }
