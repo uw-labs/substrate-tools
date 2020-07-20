3146fede39c9e26f3906e81b2f58b09a8bfd61ba
Povilas Versockas <p.versockas@gmail.com>
2020-07-20 16:18:57 +0300

    improve documentation

--- examples/flush/sink.go
+++ examples/flush/sink.go
@@ -3,15 +3,16 @@ package main
 import (
 	"context"
 	"sync"
-	"time"
 
+	"github.com/prometheus/client_golang/prometheus"
 	"github.com/uw-labs/substrate"
 	"github.com/uw-labs/substrate-tools/flush"
+	"github.com/uw-labs/substrate/instrumented"
 	"github.com/uw-labs/substrate/kafka"
 )
 
 func main() {
-	asyncSink, err := kafka.NewAsyncMessageSink(kafka.AsyncMessageSinkConfig{
+	sink, err := kafka.NewAsyncMessageSink(kafka.AsyncMessageSinkConfig{
 		Brokers: []string{"localhost:9092"},
 		Topic:   "example-topic",
 		Version: "2.0.1",
@@ -20,21 +21,28 @@ func main() {
 		panic(err)
 	}
 
-	// This context is used to control the goroutines initialised by the flushing wrapper.
-	// Canelling this context closes the underlying sink and allows `Flush` to return.
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-	defer cancel()
+	sink = instrumented.NewAsyncMessageSink(sink, prometheus.CounterOpts{
+		Name: "messages_produced_total",
+		Help: "The total count of messages produced",
+	}, "topic")
 
 	ackFn := flush.WithAckFunc(func(msg substrate.Message) error {
 		println(string(msg.Data()))
 		return nil
 	})
 
-	sink := flush.NewAsyncMessageSink(ctx, asyncSink, ackFn)
+	flushSink := flush.NewAsyncMessageSink(context.Background(), sink,
+		ackFn, flush.WithMsgBufferSize(50), flush.WithAckBufferSize(100))
+
 	defer func() {
 		// Flush will block until all messages produced using `PublishMessage` have been acked
 		// by the AckFunc, if provided.
-		err := sink.Flush()
+		err := flushSink.Flush()
+		if err != nil {
+			panic(err)
+		}
+
+		err = flushSink.Close()
 		if err != nil {
 			panic(err)
 		}
@@ -43,7 +51,7 @@ func main() {
 	go func() {
 		// Run blocks until the sink is closed or an error occurs. If the AckFn retruns an error
 		// it will be returned by `Run`.
-		err = sink.Run()
+		err = flushSink.Run()
 		if err != nil {
 			panic(err)
 		}
@@ -52,14 +60,6 @@ func main() {
 	messages := []string{
 		"message one",
 		"message two",
-		"message three",
-		"message four",
-		"message five",
-		"message six",
-		"message seven",
-		"message eight",
-		"message nine",
-		"message ten",
 	}
 
 	var wg sync.WaitGroup
@@ -72,7 +72,10 @@ func main() {
 			// This context is used to control the publishing of the message. This ctx
 			// could, and probably should, be different to the lifecyle context passed
 			// into the constructor.
-			sink.PublishMessage(context.Background(), []byte(msg))
+			err := flushSink.PublishMessage(context.Background(), []byte(msg))
+			if err != nil {
+				panic(err)
+			}
 		}(msg)
 	}
 
